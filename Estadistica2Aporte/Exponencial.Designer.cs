c0b7c0c394fe23e0a28544536965ad6ab05bae99
frangusdj <frangusdj@hotmail.com>
2020-06-18 14:59:18 -0500

    Distribucion exponencial

--- Estadistica2Aporte/Exponencial.Designer.cs
+++ Estadistica2Aporte/Exponencial.Designer.cs
@@ -32,24 +32,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.txtLambda = new System.Windows.Forms.TextBox();
+            this.txtLambdaN = new System.Windows.Forms.TextBox();
             this.txtX = new System.Windows.Forms.TextBox();
-            this.txtEx = new System.Windows.Forms.TextBox();
-            this.txtVx = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtProbabilidad = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rBtnMayorIgual = new System.Windows.Forms.RadioButton();
+            this.rBtnMenorIgual = new System.Windows.Forms.RadioButton();
+            this.rBtnEntre = new System.Windows.Forms.RadioButton();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.btnBorrar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtInterInicial = new System.Windows.Forms.TextBox();
+            this.txtInterFinal = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -70,7 +67,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(113, 145);
+            this.label2.Location = new System.Drawing.Point(113, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(165, 17);
             this.label2.TabIndex = 1;
@@ -81,33 +78,11 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(113, 185);
+            this.label3.Location = new System.Drawing.Point(113, 207);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(162, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Nº de Ocurrencia (x):";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(113, 221);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 17);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "E(x):";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(113, 260);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 17);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "V(x):";
             // 
             // label6
             // 
@@ -120,40 +95,26 @@
             this.label6.TabIndex = 5;
             this.label6.Text = "Probabilidad:";
             // 
-            // txtLambda
+            // txtLambdaN
             // 
-            this.txtLambda.Location = new System.Drawing.Point(276, 145);
-            this.txtLambda.Name = "txtLambda";
-            this.txtLambda.Size = new System.Drawing.Size(201, 22);
-            this.txtLambda.TabIndex = 1;
+            this.txtLambdaN.Location = new System.Drawing.Point(276, 167);
+            this.txtLambdaN.Name = "txtLambdaN";
+            this.txtLambdaN.Size = new System.Drawing.Size(201, 22);
+            this.txtLambdaN.TabIndex = 1;
             // 
             // txtX
             // 
-            this.txtX.Location = new System.Drawing.Point(276, 182);
+            this.txtX.Location = new System.Drawing.Point(276, 204);
             this.txtX.Name = "txtX";
             this.txtX.Size = new System.Drawing.Size(201, 22);
-            this.txtX.TabIndex = 2;
+            this.txtX.TabIndex = 3;
             // 
-            // txtEx
+            // txtProbabilidad
             // 
-            this.txtEx.Location = new System.Drawing.Point(276, 221);
-            this.txtEx.Name = "txtEx";
-            this.txtEx.Size = new System.Drawing.Size(201, 22);
-            this.txtEx.TabIndex = 3;
-            // 
-            // txtVx
-            // 
-            this.txtVx.Location = new System.Drawing.Point(276, 255);
-            this.txtVx.Name = "txtVx";
-            this.txtVx.Size = new System.Drawing.Size(201, 22);
-            this.txtVx.TabIndex = 4;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(276, 413);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(201, 22);
-            this.textBox5.TabIndex = 10;
+            this.txtProbabilidad.Location = new System.Drawing.Point(276, 413);
+            this.txtProbabilidad.Name = "txtProbabilidad";
+            this.txtProbabilidad.Size = new System.Drawing.Size(201, 22);
+            this.txtProbabilidad.TabIndex = 10;
             // 
             // pictureBox1
             // 
@@ -169,12 +130,9 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox1.Controls.Add(this.radioButton6);
-            this.groupBox1.Controls.Add(this.radioButton5);
-            this.groupBox1.Controls.Add(this.radioButton4);
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.rBtnMayorIgual);
+            this.groupBox1.Controls.Add(this.rBtnMenorIgual);
+            this.groupBox1.Controls.Add(this.rBtnEntre);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 295);
             this.groupBox1.Name = "groupBox1";
@@ -183,71 +141,38 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Operaciones";
             // 
-            // radioButton6
+            // rBtnMayorIgual
             // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(464, 60);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(127, 21);
-            this.radioButton6.TabIndex = 5;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "Mayor o igual";
-            this.radioButton6.UseVisualStyleBackColor = true;
+            this.rBtnMayorIgual.AutoSize = true;
+            this.rBtnMayorIgual.Location = new System.Drawing.Point(459, 43);
+            this.rBtnMayorIgual.Name = "rBtnMayorIgual";
+            this.rBtnMayorIgual.Size = new System.Drawing.Size(127, 21);
+            this.rBtnMayorIgual.TabIndex = 5;
+            this.rBtnMayorIgual.Text = "Mayor o igual";
+            this.rBtnMayorIgual.UseVisualStyleBackColor = true;
+            this.rBtnMayorIgual.CheckedChanged += new System.EventHandler(this.rBtnMayorIgual_CheckedChanged);
             // 
-            // radioButton5
+            // rBtnMenorIgual
             // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(249, 60);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(128, 21);
-            this.radioButton5.TabIndex = 4;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Menor o igual";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.rBtnMenorIgual.AutoSize = true;
+            this.rBtnMenorIgual.Location = new System.Drawing.Point(217, 43);
+            this.rBtnMenorIgual.Name = "rBtnMenorIgual";
+            this.rBtnMenorIgual.Size = new System.Drawing.Size(128, 21);
+            this.rBtnMenorIgual.TabIndex = 4;
+            this.rBtnMenorIgual.Text = "Menor o igual";
+            this.rBtnMenorIgual.UseVisualStyleBackColor = true;
+            this.rBtnMenorIgual.CheckedChanged += new System.EventHandler(this.rBtnMenorIgual_CheckedChanged);
             // 
-            // radioButton4
+            // rBtnEntre
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(464, 33);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(73, 21);
-            this.radioButton4.TabIndex = 3;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Mayor";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(249, 33);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(74, 21);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Menor";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(29, 60);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(68, 21);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Entre";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(29, 33);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(64, 21);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Igual";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rBtnEntre.AutoSize = true;
+            this.rBtnEntre.Location = new System.Drawing.Point(17, 43);
+            this.rBtnEntre.Name = "rBtnEntre";
+            this.rBtnEntre.Size = new System.Drawing.Size(68, 21);
+            this.rBtnEntre.TabIndex = 1;
+            this.rBtnEntre.Text = "Entre";
+            this.rBtnEntre.UseVisualStyleBackColor = true;
+            this.rBtnEntre.CheckedChanged += new System.EventHandler(this.rBtnEntre_CheckedChanged);
             // 
             // btnCalcular
             // 
@@ -259,6 +184,7 @@
             this.btnCalcular.TabIndex = 13;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // btnBorrar
             // 
@@ -270,6 +196,43 @@
             this.btnBorrar.TabIndex = 14;
             this.btnBorrar.Text = "Borrar";
             this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(138, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Inter. Inicial:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(321, 235);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Inter. Final:";
+            // 
+            // txtInterInicial
+            // 
+            this.txtInterInicial.Location = new System.Drawing.Point(241, 232);
+            this.txtInterInicial.Name = "txtInterInicial";
+            this.txtInterInicial.Size = new System.Drawing.Size(59, 22);
+            this.txtInterInicial.TabIndex = 6;
+            // 
+            // txtInterFinal
+            // 
+            this.txtInterFinal.Location = new System.Drawing.Point(418, 232);
+            this.txtInterFinal.Name = "txtInterFinal";
+            this.txtInterFinal.Size = new System.Drawing.Size(59, 22);
+            this.txtInterFinal.TabIndex = 19;
             // 
             // Exponencial
             // 
@@ -277,18 +240,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(631, 512);
+            this.Controls.Add(this.txtInterFinal);
+            this.Controls.Add(this.txtInterInicial);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.btnBorrar);
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.txtVx);
-            this.Controls.Add(this.txtEx);
+            this.Controls.Add(this.txtProbabilidad);
             this.Controls.Add(this.txtX);
-            this.Controls.Add(this.txtLambda);
+            this.Controls.Add(this.txtLambdaN);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -308,23 +271,20 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtLambda;
+        private System.Windows.Forms.TextBox txtLambdaN;
         private System.Windows.Forms.TextBox txtX;
-        private System.Windows.Forms.TextBox txtEx;
-        private System.Windows.Forms.TextBox txtVx;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtProbabilidad;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton6;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rBtnMayorIgual;
+        private System.Windows.Forms.RadioButton rBtnMenorIgual;
+        private System.Windows.Forms.RadioButton rBtnEntre;
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtInterInicial;
+        private System.Windows.Forms.TextBox txtInterFinal;
     }
 }
--- Estadistica2Aporte/Exponencial.cs
+++ Estadistica2Aporte/Exponencial.cs
@@ -8,13 +8,104 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Estadistica2Aporte
-{
-    public partial class Exponencial : Form
-    {
-        public Exponencial()
-        {
+namespace Estadistica2Aporte{
+    public partial class Exponencial : Form{
+        public Exponencial(){
             InitializeComponent();
+            Desactivar();
+        }
+        //Método para borrar
+        private void Borrar() {
+            txtLambdaN.Text = "";
+            txtX.Text = "";            
+            txtInterInicial.Text = "";
+            txtInterFinal.Text = "";
+            txtProbabilidad.Text = "";
+        }
+        //Método para desactivar cajas de texto innecesarias
+        private void Desactivar() {
+            txtInterInicial.Enabled = false;
+            txtInterFinal.Enabled = false;
+            txtX.Enabled = true;
+        }
+        //Método para activar las cajas de texto
+        private void Activar() {
+            txtInterInicial.Enabled = true;
+            txtInterFinal.Enabled = true;
+            txtX.Enabled = false;
+        }
+        //Método para calcular la distribición exponencial
+        private double DistExponencial(double l, double x) {
+            return (1-Math.Pow(Math.E,(-1*(l*x))));
+        }
+        //Método para validar Lambda numerador
+        private bool ValLambdaN() {
+            if (Convert.ToDouble(txtLambdaN.Text) > 0){
+                return true;
+            }else {
+                return false;
+            }
+        }
+        //Método para validar x
+        private bool ValOcurrencia() {
+            if (Convert.ToDouble(txtX.Text) > 0){
+                return true;
+            }else {
+                return false;
+            }
+        }
+        private void btnCalcular_Click(object sender, EventArgs e){
+            double r = 0;
+            try{
+                if (ValLambdaN() == true){
+                    if (ValOcurrencia() == true) {
+                        if (rBtnEntre.Checked){
+                            r = DistExponencial(Convert.ToDouble(txtLambdaN.Text), Convert.ToDouble(txtInterFinal.Text))
+                                - DistExponencial(Convert.ToDouble(txtLambdaN.Text), Convert.ToDouble(txtInterInicial.Text));
+                            txtProbabilidad.Text = r.ToString();
+                        }else if (rBtnMenorIgual.Checked) {
+                            txtProbabilidad.Text = DistExponencial(Convert.ToDouble(txtLambdaN.Text),Convert.ToDouble(txtX.Text)).ToString();
+                        }else if (rBtnMayorIgual.Checked) {
+                            r = 1 - DistExponencial(Convert.ToDouble(txtLambdaN.Text),Convert.ToDouble(txtX.Text));
+                            txtProbabilidad.Text = r.ToString();
+                        }
+                    }else {
+                        txtProbabilidad.Text = "0";
+                    }
+                }else {
+                    MessageBox.Show("Ingrese el promedio de ocurrencia Lambda"
+                        +"\nEste valor tiene que ser mayor a 0");
+                    txtLambdaN.Text = "";
+                    txtLambdaN.Focus();
+                }
+            }catch (Exception){
+                if (txtLambdaN.Text == string.Empty) {
+                    MessageBox.Show("Ingrese el valor de Lambda"
+                        + "\nEL valor de Lambda es mayor que 0");
+                    txtLambdaN.Text = "";
+                    txtLambdaN.Focus();
+                } else if (txtX.Text == string.Empty) {
+                    MessageBox.Show("Ingrese el valor de ocurrencia");
+                    txtX.Text = "";
+                    txtX.Focus();
+                }
+            }
+        }
+
+        private void rBtnEntre_CheckedChanged(object sender, EventArgs e){
+            Activar();
+        }
+        
+        private void rBtnMenorIgual_CheckedChanged(object sender, EventArgs e){
+            Desactivar();
+        }
+
+        private void rBtnMayorIgual_CheckedChanged(object sender, EventArgs e){
+            Desactivar();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e){
+            Borrar();
         }
     }
 }
